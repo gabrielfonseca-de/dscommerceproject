a49c4f34c9922fb8c82efa2ac20f77534f410174
gabriel.fonseca-de <gabriel.fonseca.on@gmail.com>
2024-05-23 19:47:01 -0300

    Creating users

--- src/main/java/com/project/electronics/ecommerce/dscommerce/entities/Order.java
+++ src/main/java/com/project/electronics/ecommerce/dscommerce/entities/Order.java
@@ -11,8 +11,26 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
     private OrderStatus status;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order(){
+
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.payment = payment;
+    }
 
     public Long getId() {
         return id;
@@ -46,7 +64,4 @@ public class Order {
         this.client = client;
     }
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
 }
